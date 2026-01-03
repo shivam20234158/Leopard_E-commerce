bf789009f600a7be488ce42d856bbdfc3a764ad9
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2026-01-03 23:38:08 +0530

    updated analytics

--- frontend/src/components/AnalyticsTab.jsx
+++ frontend/src/components/AnalyticsTab.jsx
@@ -71,7 +71,7 @@ const AnalyticsTab = () => {
 				<ResponsiveContainer width='100%' height={400}>
 					<LineChart data={dailySalesData}>
 						<CartesianGrid strokeDasharray='3 3' />
-						<XAxis dataKey='name' stroke='#D1D5DB' />
+						<XAxis dataKey='date' stroke='#D1D5DB' />
 						<YAxis yAxisId='left' stroke='#D1D5DB' />
 						<YAxis yAxisId='right' orientation='right' stroke='#D1D5DB' />
 						<Tooltip />
