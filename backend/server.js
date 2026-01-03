668e37e8479b5eabdbaca1b3e141e8fc88f13d86
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2026-01-03 23:55:12 +0530

    error resolved2

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app=express();
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5001;
 
 const __dirname=path.resolve();
 
@@ -37,7 +37,7 @@ app.use("/api/analytics",analyticsRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
+	app.get("(.*)", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
